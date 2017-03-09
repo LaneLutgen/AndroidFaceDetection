cab1a2088b9f98ddebdda467b661d766a5eedb3e
Lane Lutgen <lutgenlane@gmail.com>
2017-03-08 17:52:39 -0700

    Implemented opencv solution

--- app/src/main/java/com/example/android/facedetection/MainActivity.java
+++ app/src/main/java/com/example/android/facedetection/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v)
     {
-        Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+        Intent intent = new Intent(MainActivity.this, OpenCVActivity.class);
         startActivity(intent);
     }
 }
